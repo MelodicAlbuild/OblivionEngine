dc80bcbc06c24f475c94d095f6fb622b7fa30605
MelodicAlbuild <ralexdrum@gmail.com>
2025-03-26 02:13:45 -0500

    fix: using outdated font system

--- OblivionEngine/src/Render/Font/OblivionFontManager.cs
+++ OblivionEngine/src/Render/Font/OblivionFontManager.cs
@@ -1,4 +1,6 @@
-﻿using SDL2;
+﻿using System.Drawing;
+using System.Drawing.Text;
+using SDL2;
 
 namespace OblivionEngine.Render.Font;
 
@@ -7,11 +9,21 @@ public class OblivionFontManager
     public static OblivionFontManager _instance;
 
     public IntPtr Font;
+    public IntPtr DiagFont;
+
+    public int FontPtSize = 24;
+    public int DiagFontPtSize = 56;
 
     public OblivionFontManager(string path)
     {
         _instance = this;
         
         Font = SDL_ttf.TTF_OpenFont(path, 24);
+        DiagFont = SDL_ttf.TTF_OpenFont(path, 56);
+
+        if (SDL.SDL_GetError() != "")
+        {
+            throw new Exception("Failed to open font: " + SDL.SDL_GetError());
+        }
     }
 }
